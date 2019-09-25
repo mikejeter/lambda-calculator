1ba3ec577151e86560d1a9e27336dca0bba8860d
mikejeter <mjeter45@yahoo.com>
2019-09-25 15:44:04 -0400

    modified NumberButton.js, Numbers.js, OperatorButton.js, Operators.js

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const NumberButton = () => {
-  return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
-};
+const NumberButton = ({ number, handleNumButton, }) => {
+
+  if (number === 0) {
+ return (<button className="zero number-buttons buttons" value ={number} onClick={handleNumButton}>{number}</button>);
+} else {return (
+   <>
+     {/* Display a button element rendering the data being passed down from the parent container on props */}
+     
+     {
+       <button className="number-buttons buttons" value ={number} onClick={handleNumButton}>{number}</button>
+     }
+     
+   </>
+ );}
+}
+export default NumberButton;
